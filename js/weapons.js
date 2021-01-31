ac8578da538119684045c13a1e09f309fc7f57b7
AfterPulseManager <67104421+AfterPulseManager@users.noreply.github.com>
2021-01-31 16:16:10 +0900

    Add files via upload

--- js/weapons.js
+++ js/weapons.js
@@ -12,108 +12,93 @@
       var dir_type = document.getElementById("type").value;
       
       
-      var weaponsUrl = checkUrl(dir_series, dir_rank, dir_type);
-        
-      if(Object.keys(weaponsUrl).length == 0) {
-        document.getElementById("image").style.visibility = "visible";
-        document.getElementById("next").disabled = "true";
-        document.getElementById("back").disabled = "true";
-        document.getElementById("button").disabled = "true";
-        document.getElementById("image").src = "./images/weapons/noImage.jpg";
-        document.getElementById("bigImage").href = "./images/weapons/noImage.jpg";
-    
-      }else if(Object.keys(weaponsUrl).length == 1){
-        document.getElementById("image").style.visibility = "visible";
-        document.getElementById("next").disabled = "true";
-        document.getElementById("back").disabled = "true";
-        document.getElementById("button").disabled = "true";
-        document.getElementById("image").src = weaponsUrl[0];
-        document.getElementById("bigImage").href = weaponsUrl[0];
-          
-      }else{
-        document.getElementById("image").style.visibility = "visible";
-        document.getElementById("next").disabled = "";
-        document.getElementById("back").disabled = "true";
-        document.getElementById("button").disabled = "true";
-        document.getElementById("image").src = weaponsUrl[0];
-        document.getElementById("bigImage").href = weaponsUrl[0];
-      }
-        
+      var weaponsInfo = searchWeaponsInfo(dir_series, dir_rank, dir_type);
+      
+      createLink(weaponsInfo);
+      
+
       document.getElementById("series").setAttribute("disabled", true);
       document.getElementById("rank").setAttribute("disabled", true);
       document.getElementById("type").setAttribute("disabled", true);
     }
-    
-    function imageChanger(num) {
-    
-      var dir_series = document.getElementById("series").value;
-      var dir_rank = document.getElementById("rank").value;
-      var dir_type = document.getElementById("type").value;
-  
-      var weaponsUrl = checkUrl(dir_series, dir_rank, dir_type);
-      
-      count += num;
-      
-      document.getElementById("image").src = weaponsUrl[count];
-      document.getElementById("bigImage").href = weaponsUrl[count];
-      document.getElementById("button").disabled = "true";
-      
-      if(count == weaponsUrl.length - 1){
-        document.getElementById("next").disabled = "true";
-        document.getElementById("back").disabled = "";
-
-      }else if(0 < count && count < weaponsUrl.length - 1){
-        document.getElementById("next").disabled = "";
-        document.getElementById("back").disabled = "";
-
-      }else if(count <= 0){
-
-        document.getElementById("next").disabled = "";
-        document.getElementById("back").disabled = "true";
-      }
-    }
-    
-   function checkUrl(dir_series, dir_rank, dir_type){
+ 
+   function createLink(weaponsInfo){
+     var imgObj;
+     var weaponsName;
+   
+     if (weaponsInfo.length == 2){
+       imgObj = weaponsInfo[0];
+       weaponsName = weaponsInfo[1];
+       
+       var ul = document.getElementById('picturelink');
+       
+       for (var count = 0; count < imgObj.length; count++) {
+       
+         var li = document.createElement('li');
+         var text = document.createTextNode(weaponsName[count]);
+         
+         li.appendChild(text);
+         ul.appendChild(li);
+       }
+       
+     } else {
+       alert("error : plese report to administrator.");
+     }
+   }
+ 
+   function searchWeaponsInfo(dir_series, dir_rank, dir_type){
+       var weaponsInfo = new Array();
        var imgObj = new Array();
+       var weaponsName = new Array();
        
        $.ajaxSetup({async: false});
        $.getJSON("json/weapons.json", function(weapons){
           for (i=0; i<weapons.length; i++) {
-            if(dir_series == "ZERO" && dir_rank == "ZERO" && dir_type == "ZERO" && weapons[i].exsist == 1){
+            if (dir_series == "ZERO" && dir_rank == "ZERO" && dir_type == "ZERO" && weapons[i].exsist == 1){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
             }else if (dir_series == "ZERO" && dir_rank== "ZERO" && weapons[i].exsist == 1){
-              if(dir_type == weapons[i].type){
+              if (dir_type == weapons[i].type){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
-            }else if(dir_series == "ZERO" && dir_type== "ZERO" && weapons[i].exsist == 1){
-              if(dir_rank == weapons[i].rank){
+            }else if (dir_series == "ZERO" && dir_type== "ZERO" && weapons[i].exsist == 1){
+              if (dir_rank == weapons[i].rank){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
-            }else if(dir_rank == "ZERO" && dir_type== "ZERO" && weapons[i].exsist == 1){
-              if(dir_series == weapons[i].series){
+            }else if (dir_rank == "ZERO" && dir_type== "ZERO" && weapons[i].exsist == 1){
+              if (dir_series == weapons[i].series){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
-            }else if(dir_series == "ZERO" && weapons[i].exsist == 1){
-              if(dir_rank == weapons[i].rank && dir_type == weapons[i].type){
+            }else if (dir_series == "ZERO" && weapons[i].exsist == 1){
+              if (dir_rank == weapons[i].rank && dir_type == weapons[i].type){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
-            }else if(dir_rank == "ZERO" && weapons[i].exsist == 1){
-              if(dir_series == weapons[i].series && dir_type == weapons[i].type){
+            }else if (dir_rank == "ZERO" && weapons[i].exsist == 1){
+              if (dir_series == weapons[i].series && dir_type == weapons[i].type){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
-            }else if(dir_type == "ZERO" && weapons[i].exsist == 1){
-              if(dir_series == weapons[i].series && dir_rank == weapons[i].rank){
+            }else if (dir_type == "ZERO" && weapons[i].exsist == 1){
+              if (dir_series == weapons[i].series && dir_rank == weapons[i].rank){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
             }else{
-              if(dir_series == weapons[i].series && dir_rank == weapons[i].rank && dir_type == weapons[i].type){
+              if (dir_series == weapons[i].series && dir_rank == weapons[i].rank && dir_type == weapons[i].type){
                 imgObj.push(weapons[i].url);
+                weaponsName.push(weapons[i].name);
               }
             }
           }
         });
         $.ajaxSetup({async: true});
         
-        return imgObj;
+        weaponsInfo.push(imgObj);
+        weaponsInfo.push(weaponsName);
+        
+        return weaponsInfo;
    }
-   
