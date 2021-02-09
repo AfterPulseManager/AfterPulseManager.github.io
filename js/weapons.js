9f40c9647e7b1e08528fafbf042f35bade583ed5
AfterPulseManager <67104421+AfterPulseManager@users.noreply.github.com>
2021-02-09 22:33:02 +0900

    Add files via upload

--- js/weapons.js
+++ js/weapons.js
@@ -35,7 +35,7 @@
        for (var count = 0; count < imgObj.length; count++) {
        
          var li = document.createElement("li");
-         var text = document.createTextNode("<a href=\"" + imgObj[count]+ "\" target=\"_blank\" rel=\"noopener noreferrer\">"+ weaponsName[count] +"</a>");
+         var text = document.createTextNode(weaponsName[count]);
          
          li.appendChild(text);
          ul.appendChild(li);
